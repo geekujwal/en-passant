91dee4ca9cf42d51f3ce0a069b40e14e01ab66fc
Pranij <pranij@veelapp.com>
2024-12-06 14:19:40 +0545

    initial commit

--- Lilith/Controllers/WeatherForecastController.cs
+++ Lilith/Controllers/WeatherForecastController.cs
@@ -22,14 +22,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        Console.WriteLine("hello");
         throw new NotFoundException("Not found");
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            Console.WriteLine("hello");
-        })
-        .ToArray();
     }
 }
